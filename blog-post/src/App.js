a191a43743175f967018a2f182a6c8d703acbb23
Danny Chang <dannychachang@gmail.com>
2018-07-13 15:02:10 -0400

    states finished

--- blog-post/src/App.js
+++ blog-post/src/App.js
@@ -6,6 +6,26 @@ import Author from './Author.js';
 
 class Post extends Component {
 
+  constructor (props) {
+    super()
+    this.state = {
+      body: props.body
+    }
+  }
+  changeBody(e) {
+    let newBody = prompt("What should the new body be?")
+    this.setState({
+      body: newBody
+    })
+  }
+
+  handleFormInput (e) {
+    console.log(e);
+    this.setState({
+      body: e.target.value
+    })
+  }
+
   render() {
     let allComments = [
       <Comment body={this.props.comments[0]}/>,
@@ -24,7 +44,9 @@ class Post extends Component {
       <div>
         <h1>{this.props.title}</h1>
         <h2>{authors}</h2>
-        <h3>There is a body property here: {this.props.body}</h3>
+        <p>{this.state.body}</p>
+        <button onClick={(e) => this.changeBody(e)}>Edit Body</button>
+        <input type="text" onChange={(e) => this.handleFormInput(e)}/>
         <h4>Comments</h4>
           {allComments}
       </div>
